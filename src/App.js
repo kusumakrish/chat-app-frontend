3ec203b062c46015f52bff259f815af2d9c75ded
kusumakrishna <kusumakrishna524@gmail.com>
2022-09-19 15:04:41 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client"
 import { useState } from 'react';
 import Chat from './chat';
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://kusumakrishna.herokuapp.com/");
 
 function App() {
 
